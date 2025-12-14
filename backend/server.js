7c51e7e11d7021ff237226d5ab20180205c04c37
rohan <bhawarerohan24@gmail.com>
2025-12-14 13:15:37 +0530

    Trigger backend redeploy

--- backend/server.js
+++ backend/server.js
@@ -28,3 +28,9 @@ app.get("/", (req, res)=> res.send("API is running"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> console.log(`Server running on ${PORT}`));
+
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
+
